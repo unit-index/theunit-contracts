4e53a33bbeca4697494597e9779937007643d37f
ShuaJJ <Sody008@gmail.com>
2023-11-25 10:13:37 +0800

    fixed mint ticket issue

--- script/MintTicket.s.sol
+++ script/MintTicket.s.sol
@@ -4,12 +4,16 @@ pragma solidity >=0.8.21 <0.9.0;
 import { BaseScript } from "./Base.s.sol";
 import { console2 } from "forge-std/console2.sol";
 import { ITicketFactory } from "../src/interfaces/ITicketFactory.sol";
+import { Unit } from "../src/core/Unit.sol";
 
 contract MintTicket is BaseScript {
     function run(address ticket, address to, uint256 amount) public broadcast returns (bool) {
 
         address ticketFactoryAddress = _getContractAddress("Ticket");
         ITicketFactory ticketFactory = ITicketFactory(ticketFactoryAddress);
+
+        Unit un = Unit(bridgedUN);
+        un.approve(ticketFactoryAddress, amount * 1e18);
         ticketFactory.lock(ticket, to, amount * 1e18);
         return true;
     }
