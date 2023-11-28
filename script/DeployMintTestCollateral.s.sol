531996e9d6170a6ba3e689615d1b4361fcce2e2e
ShuaJJ <Sody008@gmail.com>
2023-11-28 11:04:39 +0800

    Added test collateral token

---
+++ script/DeployMintTestCollateral.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.21 <0.9.0;
+
+import { BaseScript } from "./Base.s.sol";
+import { TestCollateral } from "../src/test/TestCollateral.sol";
+import { TicketFactory } from "../src/core/TicketFactory.sol";
+import { console2 } from "forge-std/console2.sol";
+
+contract DeployMintTestCollateral is BaseScript {
+    function run(address to, uint256 amount) public broadcast returns (bool) {
+
+        TestCollateral collateral = new TestCollateral(broadcaster);
+        console2.log("Test Collateral deployed at: ", address(collateral));
+        collateral.mint(to, amount * 1e18);
+
+        return true;
+    }
+}
