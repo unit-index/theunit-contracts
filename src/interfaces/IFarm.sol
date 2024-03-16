6f7a3dff9c8de51b61444894b6951f1186f6cbf9
zhoushuren <zhoujun247@foxmail.com>
2024-03-16 14:27:29 +0800

    add interface

--- src/interfaces/IFarm.sol
+++ src/interfaces/IFarm.sol
@@ -4,5 +4,7 @@
 pragma solidity ^0.8.21;
 
 interface IFarm {
-     function depositAndLock(uint256 _pid, uint256 _multiplierIndex, uint256 _amount, address _to) external;
+     function depositETH(uint8 lock) external;
+     function deposit(address _depositToken, uint256 _amount, uint8 _lockDay) external;
+     function withdraw(address _uLP, uint256 _lockIndex, address _receiver) external; 
 }
